c4b6f12f79ddbadd3ae3da5284dc7e6d29d8af2b
Daniel <danielnun9719@gmail.com>
2021-06-20 16:25:00 -0500

    Commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,16 @@
 const seccionesPaginas = new fullpage('#fullpage',{
-    // ──────────────────────────────────────────────────
-	//   :::::: Opciones Basicas
-	// ──────────────────────────────────────────────────
 		 autoScrolling: true, // Se activa el scroll.
-		 fitToSection: false, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
+		 fitToSection: true, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
 		 fitToSectionDelay: 300, // Delay antes de acomodar la seccion automaticamente.
 		 easing: 'easeInOutCubic', // Funcion de tiempo de la animacion.
 		 scrollingSpeed: 700, // Velocidad del scroll. Valores: en milisegundos.
 		 css3: true, // Si usara CSS3 o javascript.
 		 easingcss3: 'ease-out', // Curva de velocidad del efecto.
 		 loopBottom: false, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
-
-
-         	// ──────────────────────────────────────────────────
-	//   :::::: Barra de navegación
-	// ──────────────────────────────────────────────────
 		 navigation: true, // Muesta la barra de navegación.
 		 menu: '#menu', // Menu de navegación.
-		 anchors: ['inicio', 'sobre', 'productos', 'contacto'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
-		 navigationTooltips: ['Inicio', 'Sobre', 'Productos', 'Contacto'], // Tooltips que mostrara por cada boton.
-		 showActiveTooltip: true, // Mostrar tooltip activa.
+		 anchors: ['home', 'about', 'products', 'contact'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
+		 navigationTooltips: ['Inicio', 'obre', 'Productos', 'Contacto'], // Tooltips que mostrara por cada boton.
+		 showActiveTooltip: false, // Mostrar tooltip activa.
+		 verticalCentered: false,
 });
